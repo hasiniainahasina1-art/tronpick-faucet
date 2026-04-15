03f19eaae790554cd694670699c596fea801fe56
hasiniainahasina1-art <hasiniainahasina1@gmail.com>
2026-04-15 21:02:34 +0300

    Update accounts.js

--- api/accounts.js
+++ api/accounts.js
@@ -1,3 +1,4 @@
+// api/accounts.js
 export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
