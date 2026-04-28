695d099931e1c3eafbbb983a7cc471d53a630ce2
hasiniainahasina1-art <hasiniainahasina1@gmail.com>
2026-04-28 06:07:49 +0300

    Update config.js

--- api/config.js
+++ api/config.js
@@ -2,6 +2,9 @@
 export default function handler(req, res) {
     res.status(200).json({
         supabaseUrl: process.env.SUPABASE_URL,
-        supabaseAnonKey: process.env.SUPABASE_ANON_KEY
+        supabaseAnonKey: process.env.SUPABASE_ANON_KEY,
+        ghUsername: process.env.GH_USERNAME,
+        ghRepo: process.env.GH_REPO,
+        ghBranch: process.env.GH_BRANCH || 'main'
     });
 }
